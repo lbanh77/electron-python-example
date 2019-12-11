8517020934a0d4f5c4096c71b92bd3e3018ee8e3
Linda Banh <45084991+lbanh77@users.noreply.github.com>
2019-12-11 07:06:22 -0800

    modify renderer.js for sum

--- renderer.js
+++ renderer.js
@@ -10,18 +10,31 @@ client.invoke("echo", "server ready", (error, res) => {
     console.log("server is ready")
   }
 })
-
-let formula = document.querySelector('#formula')
+    
+let num1 = document.querySelector('#num1')
+let num2 = document.querySelector('#num2')
 let result = document.querySelector('#result')
+let sum = document.querySelector('#btnEd')
 
-formula.addEventListener('input', () => {
-  client.invoke("calc", formula.value, (error, res) => {
-    if(error) {
-      console.error(error)
-    } else {
-      result.textContent = res
-    }
+sum.addEventListener('click', () => {
+  client.invoke("sumValues", num1.value, num2.value, (error, res) => {
+    result.textContent = res
   })
 })
 
-formula.dispatchEvent(new Event('input'))
+// sum.dispatchEvent(new Event('button'))
+
+// let formula = document.querySelector('#formula')
+// let result = document.querySelector('#result')
+
+// formula.addEventListener('input', () => {
+//   client.invoke("calc", formula.value, (error, res) => {
+//     if(error) {
+//       console.error(error)
+//     } else {
+//       result.textContent = res
+//     }
+//   })
+// })
+
+// formula.dispatchEvent(new Event('input'))
